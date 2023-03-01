254d270504967510e07ef6c08f6b1bdeb36d31c2
Rodrigopt <44873326+Rodrigopt@users.noreply.github.com>
2023-03-01 09:41:48 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,72 @@
+//const myHeading = document.querySelector("h1");
+//myHeading.textContent = "hello world";
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+let i = multiply(16, 32);
+console.log(i);
+
+
+alert(multiply(32, 32))
+
+/*document.querySelector("html").addEventListener("click", function () {
+    alert("ai pai para");
+});*/
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/oceano.jpg") {
+    myImage.setAttribute("src", "images/oceanoF.jpg");
+  } else {
+    myImage.setAttribute("src", "images/oceano.jpg");
+  }
+};
+
+/*let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+} */
+
+function setUserName() {
+  const myName = prompt("Por favor digite o seu nome.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla é legal, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla é legal, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
