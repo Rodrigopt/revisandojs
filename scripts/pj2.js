45550d4417ec674a4427ff83819f0e46df2f94a3
Rodrigopt <44873326+Rodrigopt@users.noreply.github.com>
2023-03-02 10:20:47 -0300

    scripts/pj2.js

---
+++ scripts/pj2.js
@@ -0,0 +1,9 @@
+function retornaMaior (a, b) {
+    if (a>b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(retornaMaior(2022,20024));
---
+++ scripts/pj3.js
@@ -0,0 +1,22 @@
+fizzBuzz(11);
+
+
+function fizzBuzz(entrada) {
+    if (entrada % 3 === 0 && entrada % 5 === 0) {
+        return console.log("FizzBuzz");
+    } else {
+        if (entrada % 3 === 0) {
+            return console.log("Fizz");
+        } else {
+            if (entrada % 5 === 0) {
+                return console.log("Buzz");
+            } else {
+                if (entrada % 1 === 0) {
+                    return console.log("Não Divisivel por 3 ou 5");
+                } else {
+                    return console.log("Não é um número");
+                }
+            }
+        }
+    }
+}
