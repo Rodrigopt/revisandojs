5238dcf22e78976f4232442e2838f59a7cd3334c
Rodrigopt <44873326+Rodrigopt@users.noreply.github.com>
2023-03-03 21:12:06 -0300

    commit 1

---
+++ scripts/pj7.js
@@ -0,0 +1,13 @@
+//somar multiplos de 3 e 5
+
+somar(10);
+
+function somar(limite) {
+    let soma=0;
+    for (i=limite;i>0;i--){
+        if (i%3===0 || i%5===0) {
+            soma=soma + i;
+        }
+    }
+    return console.log(soma);
+}
---
+++ scripts/pj8.js
@@ -0,0 +1,35 @@
+//media de array
+
+const notasAluno = [90, 70, 70];
+media(notasAluno)
+analisar(70);
+
+function media(notas) {
+    let soma = 0;
+    for (i = 0; i < notasAluno.length; i++) {
+        soma = soma + notas[i];
+    }
+    return console.log((soma / notasAluno.length));
+}
+
+function analisar(mediaNota) {
+    if (mediaNota >= 90 && mediaNota <= 100) {
+        return console.log("Nota A");
+    } else {
+        if (mediaNota >= 80 && mediaNota <= 89) {
+            return console.log("Nota B");
+        } else {
+            if (mediaNota >= 70 && mediaNota <= 79) {
+                return console.log("Nota C");
+            } else {
+                if (mediaNota >= 60 && mediaNota <= 69) {
+                    return console.log("Nota D");
+                } else {
+                    if (mediaNota >= 0 && mediaNota <= 59) {
+                        return console.log("Nota F");
+                    }
+                }
+            }
+        }
+    }
+}
